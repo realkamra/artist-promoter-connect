0ed2eb06874f37652d39a1c383b0190a66a3ccfb
realkamra <yamatomatsuda115@gmail.com>
2026-09-01 00:58:08 +0000

    Fix Convex authentication configuration

--- src/convex/auth.ts
+++ src/convex/auth.ts
@@ -1,10 +1,13 @@
-// THIS FILE IS READ ONLY. Do not touch this file unless you are correctly adding a new auth provider in accordance to the vly auth documentation
-
-import { convexAuth } from "@convex-dev/auth/server";
 import { Anonymous } from "@convex-dev/auth/providers/Anonymous";
+import { ConvexCredentials } from "@convex-dev/auth/providers/ConvexCredentials";
+import { convexAuth } from "@convex-dev/auth/server";
+
 import { emailOtp } from "./auth/emailOtp";
 
-
-export const { auth, signIn, signOut, store, isAuthenticated } = convexAuth({
-  providers: [emailOtp, Anonymous],
+export const { auth, signIn, signOut, store } = convexAuth({
+  providers: [
+    Anonymous,
+    ConvexCredentials,
+    emailOtp,
+  ],
 });
